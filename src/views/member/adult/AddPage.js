334fdd8aabde0be24aedbecf48c2be2ad4ea6a3b
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-06-17 19:09:10 +0200

    add form updated

--- src/views/member/adult/AddPage.js
+++ src/views/member/adult/AddPage.js
@@ -1,19 +1,57 @@
-import React, {useEffect} from 'react'
+// ** React Imports
+import { useRef, useState } from 'react'
 
-const AddNewModal = (props) => {
-  console.log({props})
+// ** Custom Components
+import Wizard from '@components/wizard'
 
-  useEffect(() => {
-    if (props.location.state === undefined) {
-      props.history.push("/404")
-    }
+// ** Steps
+import Address from './steps-with-validation/Address'
+import SocialLinks from './steps-with-validation/SocialLinks'
+import PersonalInfo from './steps-with-validation/PersonalInfo'
+import AccountDetails from './steps-with-validation/AccountDetails'
 
+const AddNewModal = () => {
+  // ** Ref
+  const ref = useRef(null)
+
+  // ** State
+  const [stepper, setStepper] = useState(null)
+  const [formData, setformData] = useState({})
+
+  const steps = [
+    {
+      id: 'personal-info',
+      title: 'Personal Info',
+      subtitle: 'Add Personal Info',
+      content: <PersonalInfo formData={formData} setformData={setformData}  stepper={stepper} />
+    },
+    {
+      id: 'account-details',
+      title: 'Account Details',
+      subtitle: 'Enter Your Account Details.',
+      content: <AccountDetails formData={formData} setformData={setformData}  stepper={stepper} />
+    },
    
-  }, [props])
+    {
+      id: 'step-address',
+      title: 'Address',
+      subtitle: 'Add Address',
+      content: <Address formData={formData} setformData={setformData}  stepper={stepper} />
+    },
+    {
+      id: 'social-links',
+      title: 'Social Links',
+      subtitle: 'Add Social Links',
+      content: <SocialLinks formData={formData} setformData={setformData}  stepper={stepper} />
+    }
+  ]
 
+  console.log({formData})
 
   return (
-    <div>Add new mwmbwe</div>
+    <div className='horizontal-wizard'>
+      <Wizard instance={el => setStepper(el)} ref={ref} steps={steps} />
+    </div>
   )
 }
 
